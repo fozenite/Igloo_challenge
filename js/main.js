496b1c8c08b892f1aae5e0ef1eda0e711fdec21d
Rohit <moza.rohit@gmail.com>
2017-06-23 17:33:12 -0400

    Code refactor

--- js/main.js
+++ js/main.js
@@ -1,48 +1,37 @@
 // Your code here ...
-let resObj1;
-let resObj2;
-let resObj3;
-let resObj4;
-let resObj5;
 let initialLoad = false;
 let imageLoad_Number = 5;
 
-
-let loadImages = (i) => {
-
+let loadImages = () => {
   let capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-  if(i > 0) {
-    $.ajax({
-      url: 'https://randomuser.me/api/',
-      dataType: 'json',
-      success: function(data) {
-      let myResult = data.results[0];
-      let first_name = capitalizeFirstLetter(myResult.name.first);
-      let last_name = capitalizeFirstLetter(myResult.name.last);
 
-      $(`#member-tile${i}`).find('.member-tile-pic').html(`<img src="${myResult.picture.large}" alt="user1-thumb" class="usr-thumb">`);
-      $(`#member-tile${i}`).find('.member-tile-details').html(`<span class="member-name">${first_name} ${last_name}</span>
-                                                            <span class="member-email">${myResult.email}</span>`);
-      i--;
-      loadImages(i);
+  $.ajax({
+    url: `https://randomuser.me/api/?results=${imageLoad_Number}`,
+    dataType: 'json'
+  }).done( (data) => {
+      for(i=0; i < imageLoad_Number; i++) {
+        let myResult = data.results[i];
+        let first_name = capitalizeFirstLetter(myResult.name.first);
+        let last_name = capitalizeFirstLetter(myResult.name.last);
+
+        $(`#member-tile${i+1}`).find('.member-tile-pic').html(`<img src="${myResult.picture.large}" alt="user1-thumb" class="usr-thumb">`);
+        $(`#member-tile${i+1}`).find('.member-tile-details').html(`<span class="member-name">${first_name} ${last_name}</span>
+                                                              <span class="member-email">${myResult.email}</span>`);
       }
+  });
 
-    });
-  }
 }
 
 $( document ).ready(function() {
   if(initialLoad === false ) {
-    loadImages(imageLoad_Number);
+    loadImages();
     initialLoad = true;
   }
 
   $("button").click( ( ) => {
-    loadImages(imageLoad_Number);
+    loadImages();
   });
-
-
 
 });
